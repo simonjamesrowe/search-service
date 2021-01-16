62e561d7fa7ecfb50a512e8411ef0f9a469fc286
Simon Rowe <simon.rowe@gmail.com>
2021-01-16 01:07:35 +0000

    add in logging

--- src/main/kotlin/com/simonjamesrowe/searchservice/adaptor/KafkaEventConsumer.kt
+++ src/main/kotlin/com/simonjamesrowe/searchservice/adaptor/KafkaEventConsumer.kt
@@ -7,6 +7,7 @@ import com.simonjamesrowe.model.data.Constants.TYPE_BLOG
 import com.simonjamesrowe.model.data.Event
 import com.simonjamesrowe.searchservice.mapper.BlogMapper
 import com.simonjamesrowe.searchservice.dao.BlogDocumentRepository
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.util.function.Consumer
@@ -17,15 +18,21 @@ class KafkaEventConsumer(
   private val objectMapper: ObjectMapper
 ) {
 
+  companion object {
+    val log = LoggerFactory.getLogger(KafkaEventConsumer::class.java)
+  }
+
   @Bean
   fun consumeEvent(): Consumer<List<Event>> =
     Consumer { events ->
+      log.info("Received events: $events")
       events.filter { it.model == TYPE_BLOG }
         .map { objectMapper.convertValue<Blog>(it.entry) }
         .filter { it.published }
         .map(BlogMapper::map)
         .run {
           if (isNotEmpty()) {
+            log.info("Indexing document $this")
             blogDocumentRepository.saveAll(this)
           }
         }
