2c4daf1390189aca3ca1a1032f29d1e5beefe1b6
Simon Rowe <simon.rowe@gmail.com>
2021-03-24 22:43:14 +0000

    more graalvm attempts - add native hint for more spring data classes.

--- src/main/kotlin/com/simonjamesrowe/searchservice/config/ElasticSearchConfig.kt
+++ src/main/kotlin/com/simonjamesrowe/searchservice/config/ElasticSearchConfig.kt
@@ -2,12 +2,23 @@ package com.simonjamesrowe.searchservice.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.domain.*
+import org.springframework.data.querydsl.QPageRequest
 import org.springframework.nativex.hint.TypeHint
 import java.time.LocalDate
 
 @Configuration
 @TypeHint(
-  types = [LocalDate::class]
+  types = [
+    LocalDate::class,
+    Pageable::class,
+    AbstractPageRequest::class,
+    PageRequest::class,
+    QPageRequest::class,
+    Sort::class,
+    Slice::class,
+    Page::class
+  ]
 )
 class ElasticSearchConfig {
 
