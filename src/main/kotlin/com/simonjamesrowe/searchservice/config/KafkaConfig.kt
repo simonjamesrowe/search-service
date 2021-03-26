a0e65290351a75760af3362c9db017b0f54f8e7b
Simon Rowe <simon.rowe@gmail.com>
2021-03-26 18:11:00 +0000

    inlcude type hints for kafka serializer, deserializer

--- src/main/kotlin/com/simonjamesrowe/searchservice/config/KafkaConfig.kt
+++ src/main/kotlin/com/simonjamesrowe/searchservice/config/KafkaConfig.kt
@@ -1,11 +1,18 @@
 package com.simonjamesrowe.searchservice.config
 
+import com.simonjamesrowe.model.serialization.WebhookEventDeserializer
 import org.apache.kafka.clients.admin.NewTopic
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.nativex.hint.AccessBits
+import org.springframework.nativex.hint.TypeHint
 
 @Configuration
+@TypeHint(
+  types = [WebhookEventDeserializer::class, WebhookEventDeserializer::class],
+  access = AccessBits.FULL_REFLECTION
+)
 class KafkaConfig {
 
   @Bean
