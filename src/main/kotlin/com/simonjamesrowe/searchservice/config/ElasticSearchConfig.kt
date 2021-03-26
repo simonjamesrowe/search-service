a97113277c37266741a5589001af5591676696bc
Simon Rowe <simon.rowe@gmail.com>
2021-03-26 12:36:39 +0000

    more graalvm attempts - add native hint for more spring data classes.

--- src/main/kotlin/com/simonjamesrowe/searchservice/config/ElasticSearchConfig.kt
+++ src/main/kotlin/com/simonjamesrowe/searchservice/config/ElasticSearchConfig.kt
@@ -29,7 +29,8 @@ import java.time.LocalDate
     "org.springframework.data.domain.Sort[]",
     "org.springframework.data.domain.Pageable[]",
     "org.springframework.data.domain.Page[]",
-    "org.springframework.data.elasticsearch.core.query.Query[]"
+    "org.springframework.data.elasticsearch.core.query.Query[]",
+    "org.elasticsearch.index.query.QueryBuilder[]"
   ],
   access = AccessBits.FULL_REFLECTION
 )
