3240f813194e1922dcf01c7d66b59b0efb3191ee
Simon Rowe <simon.rowe@gmail.com>
2021-03-30 06:15:41 +0100

    add all charsets for jager tracing

--- src/test/kotlin/com/simonjamesrowe/searchservice/test/SearchServiceApplicationTests.kt
+++ src/test/kotlin/com/simonjamesrowe/searchservice/test/SearchServiceApplicationTests.kt
@@ -4,9 +4,11 @@ import com.simonjamesrowe.component.test.BaseComponentTest
 import com.simonjamesrowe.component.test.elasticsearch.WithElasticsearchContainer
 import com.simonjamesrowe.component.test.kafka.WithKafkaContainer
 import org.junit.jupiter.api.Test
+import org.springframework.test.context.ActiveProfiles
 
 @WithElasticsearchContainer
 @WithKafkaContainer
+@ActiveProfiles("cms")
 class SearchServiceApplicationTests : BaseComponentTest() {
 
   init {
